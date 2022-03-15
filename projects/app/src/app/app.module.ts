ef27932a4be16cf629ca3292d028658d5587ce9c
gabrielibarbosa <ifc.gabrieli.barbosa@gmail.com>
2022-03-15 16:07:12 -0300

    fix(table): sample tabela

--- projects/app/src/app/app.module.ts
+++ projects/app/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { PoModule } from '@po-ui/ng-components';
-
+// import { PoModule } from '@po-ui/ng-components';
+import { PoModule } from '../../../ui/src/lib';
 import { AppComponent } from './app.component';
+import { SamplePoTableAirfareService } from './sample-po-table-airfare.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, FormsModule, RouterModule.forRoot([], { relativeLinkResolution: 'legacy' }), PoModule],
+  providers: [SamplePoTableAirfareService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
